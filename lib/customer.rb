af85eda768a5f40ba553d9275689225a9726cfda
Yuliana Alvarado <yalvarado924@gmail.com>
2020-11-25 08:28:24 -0600

    commit tree a4be9235317dcea8468be57f8dadf13844eb5e86

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
-  
+
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip = 0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|c| c.customer == self}
+    end
+
+    def waiters
+        meals.map {|meal| meal.waiter}
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
 class Waiter
 
+    attr_accessor :name, :years
+
+    @@all = []
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip = 0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|w| w.waiter == self}
+    end
+
+    def best_tipper
+        best_customer_tipper = meals.max {|meal1, meal2| meal1.tip <=> meal2.tip}
+        best_customer_tipper.customer
+    end
 end
